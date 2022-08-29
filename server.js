4cc4f84db68fc83f52f860cba97fe44ae656169e
Berserk9000 <cruz.d.kevin@gmail.com>
2022-08-29 18:08:01 -0400

    working to connect sql survur

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require ('mysql2')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,9 +8,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
+
+// Default response for any other request (Not Fprocess.envound)
 app.use((req, res) => {
   res.status(404).end();
+});
+
+//Conect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MYSQL USERNAME,
+        user: 'root',
+        //  SQL PASS
+        password:'',
+        database: 'election'
+    },
+    console.log ('Connected to the election database.')
+);
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 app.listen(PORT, () => {
